ae4534d2bfee972bb7c7382fd372e2fa58729d0f
royrubin1 <royrubin@protonmail.com>
2023-11-29 16:35:30 +0100

    Metodo entrar hecho por Elys añadido

--- src/main/Parking.java
+++ src/main/Parking.java
@@ -1,5 +1,26 @@
 package main;
 
-public class Parking {
+import java.util.ArrayList;
 
+public class Parking {
+	final int limite = 10;
+	ArrayList<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+	synchronized void entrar(Vehiculo vehiculo) {
+		while(vehiculos.size() >= limite) {
+			System.out.println("Parking lleno.");
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		vehiculos.add(vehiculo);
+		notifyAll();
+	}
+	
+	synchronized void salir() {
+		
+	}
+	
 }
