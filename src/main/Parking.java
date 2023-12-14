dfdefd9be3a3d73ed4bffc2a316118fc629da7d8
royrubin1 <royrubin@protonmail.com>
2023-12-14 17:25:55 +0100

    SalidaParking ha sido añadido

--- src/main/Parking.java
+++ src/main/Parking.java
@@ -1,47 +1,60 @@
 package main;
 
-import java.util.ArrayList;
-
 public class Parking {
-	private int limite ;
-	private Vehiculo [] parking;
-	
-	public Parking(int limite) {
 
-		this.limite = limite;
-		parking = new Vehiculo [limite];
+	private int limite;
+	private Vehiculo[] parking;
+
+	public Parking(int limite) {
+		this.setLimite(limite);
+		parking = new Vehiculo[limite];
 	}
 
 	public synchronized void entradaParking(Vehiculo vehiculo) throws InterruptedException {
-		 esperaplaza();
-		
-		 for(int i = 0; i<parking.length;i++) {
-			 if(parking[i] == null) {
-				 parking[i] = vehiculo;
-				 break;
-			 }
-		 }
-		 
+		int plaza = -1;
+		try {
+			plaza = esperaPlaza();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		parking[plaza] = vehiculo;
+		plaza = -1;
 		notifyAll();
 	}
-	
-	synchronized void salidaParking() {
-		
-	}
-	
-	private boolean estalleno() {
-		for(int i = 0; i < parking.length; i++) {
-			if(parking[i] == null) {
-				return false;
+
+	synchronized void salidaParking(Vehiculo vehiculo) {
+
+		for (int i = 0; i < parking.length; i++) {
+			if (parking[i] == vehiculo) {
+				parking[i] = null;
+				break;
 			}
 		}
-		return true;
+		notifyAll();
 	}
-	
-	private void esperaplaza() throws InterruptedException {
-		while(estalleno()) {
+
+	private int siLleno() {
+		for (int i = 0; i < parking.length; i++) {
+			if (parking[i] == null) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private int esperaPlaza() throws InterruptedException {
+		while (siLleno() == -1) {
 			wait();
 		}
+		return siLleno();
 	}
-	
+
+	public int getLimite() {
+		return limite;
+	}
+
+	public void setLimite(int limite) {
+		this.limite = limite;
+	}
+
 }
